1d75446d86943986b11af3715e64ea0037b85f9e
jshar <joha0013@stud.ek.dk>
2026-04-21 09:51:24 +0200

    njnj

---
+++ src/main/java/org/example/fullstackitimen/FullStackITimenApplication.java
@@ -0,0 +1,13 @@
+package org.example.fullstackitimen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackITimenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackITimenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/fullstackitimen/FullStackITimenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fullstackitimen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackITimenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
